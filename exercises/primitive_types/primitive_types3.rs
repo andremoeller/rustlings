3c9fa73d83a920f93a9887a8cc7fc45c93bb18c0
André Kang-Moeller <akm@octoml.ai>
2022-05-08 08:28:20 -0700

    Finish move semantics and primitive types

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,13 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let mut a = Vec::<i32>::new();
+
+    for number in (0..101) {
+        a.push(number)
+    }
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
